1e476da7fd8c2d538481f0a965476a94996cb933
rasikaadl <lfreez123@gmail.com>
2025-10-27 19:25:35 +1300

    disabled private cluster

--- gke.tf
+++ gke.tf
@@ -78,14 +78,14 @@ resource "google_container_cluster" "gke" {
   # Private cluster (control plane not public)
   private_cluster_config {
     enable_private_nodes    = true
-    enable_private_endpoint = true
-    master_ipv4_cidr_block  = "172.16.0.0/28"
+    enable_private_endpoint = false
   }
 
   # Allow your IP to access control plane (optional)
   master_authorized_networks_config {
     cidr_blocks {
       cidr_block = "203.211.111.126/32"  # ← Replace with your IP or VPC CIDR
+      display_name = "home-ip"
     }
   }
 
